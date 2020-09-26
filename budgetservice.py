5219eece3a1a0cb031eeef76f94f135d04f25fb7
zalman0813 <zalman0813@gmail.com>
2020-09-26 16:47:52 +0800

    refactor budge service

--- budgetservice.py
+++ budgetservice.py
@@ -8,32 +8,34 @@ class BudgetService(object):
 
         begin_month = datetime.strftime(start, "%Y%m")
         end_month = datetime.strftime(end, "%Y%m")
-
         budgetsList = self.get_budgets()
         amount = 0
+
         for budget in budgetsList:
             budget_datetime = datetime.strptime(budget.yearMonth, "%Y%m")
             budget_day = calendar.monthrange(budget_datetime.year, budget_datetime.month)[1]
 
-            if budget_datetime.year == start.year and budget_datetime.month == start.month and start.year == end.year and start.month == end.month:
+            if self.is_same_year_month(budget_datetime, start) and self.is_same_year_month(budget_datetime, end):
                 amount += round(budget.amount*(end.day - start.day + 1)/budget_day, 2)
                 return amount
 
             if self.is_same_year_month(budget_datetime, start):
                 amount += round(budget.amount * (budget_day - start.day + 1) / budget_day, 2)
 
-            if budget.yearMonth > begin_month and budget.yearMonth < end_month:
+            if self.is_in_period(begin_month, budget, end_month):
                 amount += budget.amount
 
-            if budget_datetime.year == end.year and budget_datetime.month == end.month:
+            if self.is_same_year_month(budget_datetime, end):
                 amount += round(budget.amount * end.day  / budget_day, 2)
 
         return amount
 
+    def is_in_period(self, begin_month, budget, end_month):
+        month = budget.yearMonth > begin_month and budget.yearMonth < end_month
+        return month
 
-
-    def is_same_year_month(self, budget_datetime, start):
-        month = budget_datetime.year == start.year and budget_datetime.month == start.month
+    def is_same_year_month(self, budget_datetime, year_month):
+        month = budget_datetime.year == year_month.year and budget_datetime.month == year_month.month
         return month
 
     def get_budgets(self):
